8399d8c2b46b4dd8f57f255a679708c8b64b8fab
Ronit <ronit10z@gmail.com>
2017-08-06 09:19:10 -0700

    using wss3 email

--- app/views.py
+++ app/views.py
@@ -1,10 +1,27 @@
-from flask import render_template
+from flask import request, render_template
 
 from app import app
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
-    return render_template('index.html', title='Home', user=user)
+    return "Homepage :  method use - %s" % request.method
+
+
+@app.route("/bacon", methods=["GET", "POST"])
+def bacon():
+    if request.method == "POST":
+        return "you are using POST"
+    else:
+        return "You are using GET"
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    return render_template("profile.html")
+
+
+@app.route("/id/<int:id>")
+def getId(id):
+    assert isinstance(id, int)
+    return "The id is %d" % id
